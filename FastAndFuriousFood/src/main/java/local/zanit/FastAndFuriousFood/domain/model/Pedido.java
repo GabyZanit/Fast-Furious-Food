6623f0210263c75d70deeda62220dbca213fe105
GabyZanit <gabriellyzanit.senai@gmail.com>
2025-08-06 14:12:21 -0300

    Projeto Completo

--- FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/model/Pedido.java
+++ FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/model/Pedido.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package local.gabriel.FastAndFuriousFood.domain.model;
+package local.zanit.FastAndFuriousFood.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
@@ -18,7 +18,7 @@ import java.util.List;
 
 /**
  *
- * @author ppjata
+ * @author ppjatb
  */
 @Entity
 public class Pedido {
