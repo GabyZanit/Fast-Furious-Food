7a41dcd59fbc4d3a724c4f7bef348fd301993f37
GabyZanit <gabriellyzanit.senai@gmail.com>
2025-08-06 14:13:46 -0300

    Projeto Completo

--- FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/repository/PedidoRepository.java
+++ FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/repository/PedidoRepository.java
@@ -2,17 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package local.gabriel.FastAndFuriousFood.domain.repository;
+package local.zanit.FastAndFuriousFood.domain.repository;
 
 import java.util.List;
 import java.util.Optional;
-import local.gabriel.FastAndFuriousFood.domain.model.Pedido;
-import local.gabriel.FastAndFuriousFood.domain.model.StatusPedido;
+import local.zanit.FastAndFuriousFood.domain.model.Pedido;
+import local.zanit.FastAndFuriousFood.domain.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
- * @author ppjata
+ * @author ppjatb
  */
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
     Optional<Pedido> findByStatus(StatusPedido status);
