e1cf0f57215695a528f97c362c767e43a48c4f2c
GabyZanit <gabriellyzanit.senai@gmail.com>
2025-08-06 14:14:12 -0300

    Projeto Completo

--- FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/repository/ProdutoRepository.java
+++ FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/repository/ProdutoRepository.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package local.gabriel.FastAndFuriousFood.domain.repository;
+package local.zanit.FastAndFuriousFood.domain.repository;
 
 import java.util.Optional;
-import local.gabriel.FastAndFuriousFood.domain.model.Produto;
+import local.zanit.FastAndFuriousFood.domain.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
- * @author ppjata
+ * @author ppjatb
  */
 public interface ProdutoRepository extends JpaRepository<Produto , Long> {
     Optional<Produto> findByCategoria(String categoria);
