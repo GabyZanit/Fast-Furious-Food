46064215b4f31979fa2504a93433e1c8a4c98edd
GabyZanit <gabriellyzanit.senai@gmail.com>
2025-08-06 14:13:24 -0300

    Projeto Completo

--- FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/repository/ItemPedidoRepository.java
+++ FastAndFuriousFood/src/main/java/local/zanit/FastAndFuriousFood/domain/repository/ItemPedidoRepository.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package local.gabriel.FastAndFuriousFood.domain.repository;
+package local.zanit.FastAndFuriousFood.domain.repository;
 
-import local.gabriel.FastAndFuriousFood.domain.model.ItemPedido;
+import local.zanit.FastAndFuriousFood.domain.model.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
- * @author ppjata
+ * @author ppjatb
  */
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
     
